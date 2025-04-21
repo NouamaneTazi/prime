4573f0d5ef9208c67975c5fcedaa1bd990aeea64
mike <michael.keiblinger@gmail.com>
2025-04-21 18:12:50 +0000

    fix NCCL crash on some Lambda nodes

--- scripts/launch_prime.sh
+++ scripts/launch_prime.sh
@@ -1,3 +1,9 @@
+# this fixes crashes on some NCCL versions that attempt to create a multi-node topology on some single-node cloud instances
+export NCCL_MNNVL_ENABLE=0
+export NCCL_IB_DISABLE=1
+export NCCL_SOCKET_IFNAME=lo
+export PCCL_LOG_LEVEL=DEBUG
+
 PYTORCH_CUDA_ALLOC_CONF=expandable_segments:True MASTER_ADDR=127.0.0.1 MASTER_PORT=1234 RANK=0 GPU_ORDINAL=0 WORLD_SIZE=8 ZERO_BAND_LOG_LEVEL=DEBUG python src/zeroband/train.py @configs/7B/H100.toml &
 PYTORCH_CUDA_ALLOC_CONF=expandable_segments:True MASTER_ADDR=127.0.0.1 MASTER_PORT=1234 RANK=1 GPU_ORDINAL=1 WORLD_SIZE=8 ZERO_BAND_LOG_LEVEL=DEBUG python src/zeroband/train.py @configs/7B/H100.toml &
 PYTORCH_CUDA_ALLOC_CONF=expandable_segments:True MASTER_ADDR=127.0.0.1 MASTER_PORT=1234 RANK=2 GPU_ORDINAL=2 WORLD_SIZE=8 ZERO_BAND_LOG_LEVEL=DEBUG python src/zeroband/train.py @configs/7B/H100.toml &
